bd7817b5b4e07dfeba0a49411397bad297c1ff10
NanMed <46913776+NanMed@users.noreply.github.com>
2019-02-12 12:33:56 -0600

    Initial commit

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+	public float carSpeed;
+	public float maxPos = 2.10f;
+
+
+	Vector3 position;
+	public UIManager ui;
+    // Start is called before the first frame update
+    void Start()
+    {
+		//Access of the uimanager script
+		//ui = GetComponent<UIManager>();
+		//transfomr.position is the current position of the car in the scene
+		position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Increase the value of the position related to the keys
+		position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+
+		//Limit the value of x (boundaries)
+		position.x = Mathf.Clamp(position.x, -2.1f, 2.1f);
+
+		transform.position = position;
+    
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if(other.gameObject.tag == "EnemyCar"){
+			Destroy(gameObject);
+			ui.gameOverActivated();
+		}
+	}
+}
---
+++ Assets/Scripts/TrackMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackMove : MonoBehaviour
+{
+	public float speed;
+	//Go down and go up again and again
+	Vector2 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		offset = new Vector2(0, Time.time * speed);
+		//Put the image track to move
+		GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
